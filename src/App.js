08a65dc56e248cf03574311a6d5c901841cf72ef
Hamid JR <hamidrahnama.rj@gmail.com>
2025-08-30 10:37:28 +0330

    Implement main pages: Team, Contacts, Invoices, and Form with headers and data grids

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard/Index";
-// import Team from "./pages/team";
-// import Invoices from "./pages/invoices";
-// import Contacts from "./pages/contacts";
+import Team from "./pages/team/Index";
+import Contacts from "./pages/contacts/Index";
+import Invoices from "./pages/invoices/Index";
+import Form from "./pages/form/Index";
 // import Bar from "./pages/bar";
-// import Form from "./pages/form";
 // import Line from "./pages/line";
 // import Pie from "./pages/pie";
 // import FAQ from "./pages/faq";
@@ -29,10 +29,11 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
+              <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              {/* 
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
